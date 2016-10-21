b0c29340ac482e36aab07a2b8c6f2c492c6d5fa7
L.B. Warren Liu <warrenliu403@gmail.com>
2016-10-21 10:31:36 -0700

    create method to claculate mean

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -1,15 +1,32 @@
 
 public class StepCounter {
-	
-	public StepCounter(double[] times, double[][] sensorData ){
-		
+
+	public StepCounter(double[] times, double[][] sensorData) {
+
 	}
-	
-	private static int countSteps(double[] times, double[][] sensorData){
-		
+
+	private static int countSteps(double[] times, double[][] sensorData) {
+
 	}
-	
-	private static double calculateMagnitude(double x, double y, double z){
-		return Math.sqrt(x*x + y*y + z*z);
+
+	private static double calculateMagnitude(double x, double y, double z) {
+		return Math.sqrt(x * x + y * y + z * z);
+	}
+
+	private static double[] calculateMagnitudesFor(double[][] sensorData) {
+
+	}
+
+	private static double calculateStandardDeviation(double[] arr, double mean) {
+
+	}
+
+	private static double calculateMean(double[] arr) {
+		int mean = 0;
+		for (int i = 0; i < arr.length; i++) {
+			mean += arr[i];
+		}
+
+		return mean / arr.length;
 	}
 }
