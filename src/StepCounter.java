3f5adeaa661b7f8d02e4453f1c65c5c2733fc017
L.B. Warren Liu <warrenliu403@gmail.com>
2016-10-21 10:53:59 -0700

    calculate standard deviation

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -1,12 +1,14 @@
 
 public class StepCounter {
 
-	public StepCounter(double[] times, double[][] sensorData) {
-
+	public StepCounter(String filepath, String[] columnNames, int linesToIgnore) {
+		CSVData data = new CSVData(filepath, columnNames, 1);
+		String[] columnHeaders = {"accel x", "accel Y", "accel Z"};
+		double[][] accelData = data.getMultipleColumns(columnHeaders);
 	}
 
 	private static int countSteps(double[] times, double[][] sensorData) {
-
+		
 	}
 
 	private static double calculateMagnitude(double x, double y, double z) {
@@ -14,11 +16,21 @@ public class StepCounter {
 	}
 
 	private static double[] calculateMagnitudesFor(double[][] sensorData) {
-
+		double[] magnitudes = new double[sensorData[0].length];
+		for (int i = 0; i < sensorData[0].length; i++){
+			magnitudes[i] = calculateMagnitude(sensorData[i][0], sensorData[i][1], sensorData[i][2]);
+		}
+		return magnitudes;
 	}
 
 	private static double calculateStandardDeviation(double[] arr, double mean) {
-
+		double standardDeviation = 0;
+		for (int i = 0; i < arr.length; i ++){
+			standardDeviation += (arr[i] - mean)*(arr[i] - mean);
+		}
+		standardDeviation = standardDeviation/(arr.length-1);
+		
+		return Math.sqrt(standardDeviation);
 	}
 
 	private static double calculateMean(double[] arr) {
